46be0f2b4ce328e2ed9538b64ba00eadbac8de2a
biarozka88 <j.biarozka@gmail.com>
2019-01-06 20:51:20 +0300

    created 2 files

---
+++ Profile.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+    <div>
+      <img src='https://nroer.gov.in/media/e/a/c/c4ce9ba9211d05e48f0bf447a346dde5f0c79a31c163d540ce9c282b43138.jpeg' />
+    </div>
+    <div>
+      {/* <img src='https://pbs.twimg.com/media/Cqg9A6wUIAAIRTP.jpg' /> */}
+      desriptiom
+    </div>
+    <div>
+      My posts
+      <div>New post</div>
+    </div>
+    <div>
+      <div>Post1</div>
+      <div>Post2</div>
+    </div>
+
+  </div>
+}
+
+export default Profile;
+
+
+
