037ac82c915414df44953e5bae19e6a7f430e559
biarozka88 <j.biarozka@gmail.com>
2019-01-06 20:38:03 +0300

    added new file Nav.jsx

---
+++ Nav.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Nav = () => {
+    return <nav className='nav'>
+    <div><a>Profile</a></div>
+    <div><a>Messages</a></div>
+    <div><a>News</a></div>
+    <div><a>Music</a></div>
+    <div><a>Settings</a></div>
+  </nav>
+}
+
+export default Nav;
+
+// git clone path-to-repository
+// git status
+
+
+// git config --global user.email "j.biarozka@gmail.com"
+// git config --global user.name "biarozka88"
+
+// git commit -am "added new file Nav.jsx"
